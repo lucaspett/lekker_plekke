43558d57c2bb07f0c7b603ffd8e48f2d3a6cd40c
lucaspett <lucaspettinato2018@u.northwestern.edu>
2015-07-23 16:45:15 +0200

    Added creation and display of comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :places
+  resources :places do
+    resources :comments
+  end
 
   root "places#index"
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723135311) do
+ActiveRecord::Schema.define(version: 20150723143456) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150723135311) do
     t.string   "reference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "place_id"
   end
 
   create_table "places", force: :cascade do |t|
